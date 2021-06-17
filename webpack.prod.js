ef45a50eb98c2bc0db47c3b0a26aa7327b16d6df
xiashu3 <xiashu3@jd.com>
2021-06-17 11:41:32 +0800

    for page

--- webpack.prod.js
+++
@@ -1,9 +0,0 @@
- const merge = require('webpack-merge');
- const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
- const common = require('./webpack.common.js');
-
- module.exports = merge(common, {
-   plugins: [
-     new UglifyJSPlugin()
-   ]
- });
